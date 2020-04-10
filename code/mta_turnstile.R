58b893f2e08bba57fce887c385d8980ee7c3a669
Kodi Arfer <git@arfer.net>
2020-04-10 11:10:38 -0400

    Allow restricting counts to a given range of hours.

--- code/mta_turnstile.R
+++ code/mta_turnstile.R
@@ -12,6 +12,7 @@ pairmemo.dir = file.path(data.root, "pairmemo")
 zcta.dir = "/data-belle/basemap/census/zcta"
 
 date.first = as.Date("2014-12-27")
+local.tz = "America/New_York"
 
 crs.lonlat = 4326 # https://epsg.io/4326
 crs.us.atlas = 2163 # https://epsg.io/2163
@@ -30,7 +31,7 @@ sdn = function(v, na.rm = F)
 
 turnstile = function()
   # Get the number of New York City Metropolitan Transit Authority
-  # (MTA) turnstile entries and exits by station and date.
+  # (MTA) turnstile entries and exits by station and timestamp.
   # Field descriptions:
   # https://web.archive.org/web/20190904142924/http://web.mta.info/developers/resources/nyct/turnstile/ts_Field_Description.txt
    {url.root = "http://web.mta.info/developers/data/nyct/turnstile/"
@@ -66,7 +67,42 @@ turnstile = function()
     stopifnot(all(str_detect(d$time, "\\A\\d\\d:\\d\\d:\\d\\d\\z")))
     stopifnot(all(d[,
         by = .(ca, unit, scp, station, date, time), .N]$N == 1))
-    setkey(d, ca, unit, scp, station, date, time)
+    d[, datetime := lubridate::mdy_hms(
+        paste(date, time), tz = local.tz)]
+    setkey(d, ca, unit, scp, station, datetime)
+
+    # Observations with the same `ca` but different station names
+    # should be referring to the same station. Use the latest name for
+    # each station. Beware that different `ca`s can have the same
+    # station name.
+    message("Setting station names")
+    stations = d[, by = ca,
+        .(station.name = station[which.max(date)])]
+
+    # Geolocate stations.
+    locations = rbind(
+        download(
+            "https://raw.githubusercontent.com/chriswhong/nycturnstiles/master/geocoded.csv",
+            "mta_turnstile_locations.csv",
+            fread, header = F, col.names = c(
+                "unit", "ca", "station", "linename", "division",
+                "lat", "lon"))[!is.na(lon), .(ca, lon, lat)],
+        # Add a few more station locations by hand.
+        data.table(
+            ca = c("N700", "N700A", "N701", "N701A", "N702", "N702A", "R550", "R551", "A049", "D001", "H007A", "N095A", "N098", "N330", "N539", "N601A", "PTH01", "PTH06", "PTH09", "PTH10", "PTH13", "PTH16", "PTH18", "PTH19", "PTH20", "PTH22", "R101", "R107D", "R108A", "R169", "R612"),
+            lon = c(-73.95836, -73.95836, -73.951921, -73.951921, -73.94766, -73.94766, -74.000596, -74.000596, -74.011052, -74.011717, -73.981719, -74.007983, -74.007983, -73.86161, -73.980324, -73.966291, -74.164494, -74.033998, -74.007022, -73.998802, -73.989231, -74.027647, -74.164494, -74.164494, -74.164494, -74.011417, -74.012983, -74.011417, -74.011417, -73.972363, -73.977417),
+            lat = c(40.768611, 40.768611, 40.777493, 40.777493, 40.783542, 40.783542, 40.755456, 40.755456, 40.710662, 40.635011, 40.730901, 40.709938, 40.709938, 40.729869, 40.666276, 40.764763, 40.734139, 40.726691, 40.733001, 40.734223, 40.747809, 40.735288, 40.734139, 40.734139, 40.734139, 40.71149, 40.703082, 40.71149, 40.71149, 40.79388, 40.684063)))
+    stopifnot(!anyDuplicated(locations$ca))
+    setkey(locations, ca)
+    stations[, c("lon", "lat") :=
+        locations[.(stations$ca), .(lon, lat)]]
+    # Drop unlocated stations and their associated observations.
+    message(sprintf("Dropping %d unlocated stations ",
+        sum(is.na(stations$lon))))
+    stations = stations[!is.na(lon)]
+    message(sprintf("Dropping %s observations",
+        d[, comma(sum(!(ca %in% stations$ca)))]))
+    d = d[ca %in% stations$ca]
 
     # The counts of entries and exits are cumulative per (`ca`,
     # `unit`, `scp`) tuple (at least, I think that's the
@@ -101,8 +137,7 @@ turnstile = function()
     min.pos.diffs.n = 3L
     max.diff = 100e3
 
-    vnames = c("entries", "exits")
-    l = lapply(vnames, function(vname)
+    l = sapply(c("entries", "exits"), simplify = F, function(vname)
        {message("Decumulating - ", vname)
         n.sources = nrow(d[, by = .(ca, unit, scp), 1])
         n.sources.dropped = 0
@@ -120,7 +155,7 @@ turnstile = function()
                {date.diffs = as.integer(diff(date))
                 i.keep = date.diffs <= 1 & diffs >= 0
                 n.obs.dropped <<- n.obs.dropped + sum(!i.keep)
-                .(date = date[-1][i.keep], count = diffs[i.keep])}
+                .(datetime = datetime[-1][i.keep], count = diffs[i.keep])}
             else
                {n.sources.dropped <<- n.sources.dropped + 1
                 n.obs.dropped <<- n.obs.dropped + .N
@@ -130,52 +165,24 @@ turnstile = function()
             comma(n.sources.dropped), comma(n.sources)))
         message(sprintf("Dropped %s observations of %s",
             comma(n.obs.dropped), comma(n.obs)))
+        out[, .(ca, datetime, count)]})
 
-        message("Summarizing")
-        out[, by = .(ca, date), .(
-            sources = length(unique(paste(unit, scp))),
-            count = sum(count))]})
+    c(l, list(stations = stations))}
+turnstile = pairmemo(turnstile, pairmemo.dir, mem = T)
 
-    message("Merging")
-    counts = merge(l[[1]], l[[2]], all = T,
+turnstile.daily = function(hour.start = NULL, hour.end = NULL)
+  # Create a data table of daily entries and exits per station.
+   {l = turnstile()
+    counts = sapply(c("entries", "exits"), simplify = F, function(vname)
+       l[[vname]][
+            (if (is.null(hour.start)) T else hour(datetime) >= hour.start) &
+                (if (is.null(hour.end)) T else hour(datetime) < hour.end),
+            by = .(ca, date = as.Date(datetime, tz = local.tz)),
+            .(count = sum(count))])
+    merge(counts[[1]], counts[[2]], all = T,
         by = c("ca", "date"),
-        suffixes = paste0(".", vnames))
-
-    # Observations with the same `ca` but different station names
-    # should be referring to the same station. Use the latest name for
-    # each station. Beware that different `ca`s can have the same
-    # station name.
-    message("Setting station names")
-    stations = d[, by = ca,
-        .(station.name = station[which.max(date)])]
-
-    # Geolocate stations.
-    locations = rbind(
-        download(
-            "https://raw.githubusercontent.com/chriswhong/nycturnstiles/master/geocoded.csv",
-            "mta_turnstile_locations.csv",
-            fread, header = F, col.names = c(
-                "unit", "ca", "station", "linename", "division",
-                "lat", "lon"))[!is.na(lon), .(ca, lon, lat)],
-        # Add a few more station locations by hand.
-        data.table(
-            ca = c("N700", "N700A", "N701", "N701A", "N702", "N702A", "R550", "R551", "A049", "D001", "H007A", "N095A", "N098", "N330", "N539", "N601A", "PTH01", "PTH06", "PTH09", "PTH10", "PTH13", "PTH16", "PTH18", "PTH19", "PTH20", "PTH22", "R101", "R107D", "R108A", "R169", "R612"),
-            lon = c(-73.95836, -73.95836, -73.951921, -73.951921, -73.94766, -73.94766, -74.000596, -74.000596, -74.011052, -74.011717, -73.981719, -74.007983, -74.007983, -73.86161, -73.980324, -73.966291, -74.164494, -74.033998, -74.007022, -73.998802, -73.989231, -74.027647, -74.164494, -74.164494, -74.164494, -74.011417, -74.012983, -74.011417, -74.011417, -73.972363, -73.977417),
-            lat = c(40.768611, 40.768611, 40.777493, 40.777493, 40.783542, 40.783542, 40.755456, 40.755456, 40.710662, 40.635011, 40.730901, 40.709938, 40.709938, 40.729869, 40.666276, 40.764763, 40.734139, 40.726691, 40.733001, 40.734223, 40.747809, 40.735288, 40.734139, 40.734139, 40.734139, 40.71149, 40.703082, 40.71149, 40.71149, 40.79388, 40.684063)))
-    stopifnot(!anyDuplicated(locations$ca))
-    setkey(locations, ca)
-    stations[, c("lon", "lat") :=
-        locations[.(stations$ca), .(lon, lat)]]
-    # Drop unlocated stations and their associated observations.
-    message(sprintf("Dropping %d unlocated stations ",
-        sum(is.na(stations$lon))))
-    stations = stations[!is.na(lon)]
-    message(sprintf("Dropping %s observations",
-        counts[, comma(sum(!(ca %in% stations$ca)))]))
-    counts = counts[ca %in% stations$ca]
-
-    list(counts = counts, stations = stations)}
-turnstile = pairmemo(turnstile, pairmemo.dir)
+        suffixes = paste0(".", names(counts)))}
+turnstile.daily = pairmemo(turnstile.daily, pairmemo.dir, mem = T, fst = T)
 
 station.boros = function()
   # Return a factor of boro names, with one boro for each station.
@@ -207,13 +214,13 @@ station.areas = function(colname, areas)
         else
             stop())]}
 
-relative.subway.usage = function(the.year, by)
+relative.subway.usage = function(the.year, by, ...)
   # For each date T in the given year, compute a measure of subway
   # usage comparing T to all other days that occur on the same month
   # and day of the week, but in different years. The usage count for T
   # is divided by the median in the other days to get a proportion.
   # The results are given per place and also pooled across places.
-   {counts = turnstile()$counts
+   {counts = copy(turnstile.daily(...))
 
     counts[, place := switch(by,
        boro = station.boros(),
